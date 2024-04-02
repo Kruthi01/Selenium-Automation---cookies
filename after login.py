7ef08f11d602b4d0ba2d8dd8de75fa7bf629255f
Kruthi01 <156498124+Kruthi01@users.noreply.github.com>
2024-04-02 18:05:54 +0530

    Add files via upload

---
+++ after login.py
@@ -0,0 +1,31 @@
+
+# Switching windows between google and saucedemo
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# initialize chrome webdriver
+driver = webdriver.Chrome()
+
+#opening saucedemo
+driver.get("https://www.saucedemo.com/")
+#getting cookies before login
+res=driver.getCookies()
+print("Before login cookie is:")
+#And printing them
+print(res)
+username_input = driver.find_element(By.ID, "user-name")
+password_input = driver.find_element(By.ID, "password")
+login_button = driver.find_element(By.ID, "login-button")
+username_input.send_keys("standard_user")
+password_input.send_keys("secret_sauce")
+login_button.click()
+time.sleep(3)
+#Getting cookies after login
+res1=driver.getCookies()
+print("After login cookie is:")
+#And printing them
+print(res1)
+
+
